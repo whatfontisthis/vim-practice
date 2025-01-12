a683e600a7550c6bafe7d3e9d8305371905e94d0
Woobin Macbok-Pro-M2 <woobin@microschool.kr>
2025-01-12 12:15:26 +0900

    baisc pages setup

--- app.py
+++ app.py
@@ -4,8 +4,23 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+
+@app.route("/chapter01")
+def chapter01():
+    return render_template("chapter01.html")
+
+
+@app.route("/chapter02")
+def chapter02():
+    return render_template("chapter02.html")
+
+
+@app.route("/chapter03")
+def chapter03():
+    return render_template("chapter03.html")
 
 
 if __name__ == "__main__":
